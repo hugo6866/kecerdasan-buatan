84583c2e65ee4cb7b856cac4a2684868ba3ec23d
Ciev <hafizhhugo@gmail.com>
2023-03-23 01:36:32 +0700

    add comment explanation

--- dfs.py
+++ dfs.py
@@ -15,36 +15,46 @@ maps = {
 
 
 def dfs(peta, mulai):
+    # Fungsi ini digunakan untuk mencari jalur menggunakan algoritma DFS (Depth First Search)
     resultNode = []
     tumpukan = [mulai]
     while tumpukan:
         prosesNode = tumpukan.pop()
+        # Menambahkan node yang sedang diproses ke dalam hasil
         resultNode.append(prosesNode)
         for anak in peta[prosesNode]:
             if anak not in resultNode:
+                # Menambahkan node anak ke tumpukan
                 tumpukan.append(anak)
     return resultNode
 
 
 def dfs_goal(peta, mulai, goal):
+    # Fungsi ini digunakan untuk mencari jalur ke node tujuan menggunakan algoritma DFS
     resultNode = []
     jalurNode = {}
     tumpukan = [mulai]
     while tumpukan:
-        prosesNode = tumpukan.pop()
+        prosesNode = tumpukan.pop()  # Mengambil node terakhir dari tumpukan
+        # Menambahkan node yang sedang diproses ke dalam hasil
         resultNode.append(prosesNode)
         for anak in peta[prosesNode]:
             if anak not in resultNode:
+                # Menambahkan node anak ke tumpukan
                 tumpukan.append(anak)
+                # Menyimpan jalur dari node sebelumnya ke node anak
                 jalurNode[anak] = prosesNode
     return jalurNode
 
 
 def dfs_jalur(jalurnode, mulai, tujuan):
+    # Fungsi untuk mengembalikan jalur dari node awal ke node tujuan
     jalur = [tujuan]
     prosesNode = tujuan
     while prosesNode != mulai:
+        # Mengatur node yang sedang diproses ke node sebelumnya
         prosesNode = jalurnode[prosesNode]
+        # Menambahkan node yang sedang diproses ke jalur
         jalur.append(prosesNode)
     return jalur
 
